340168c157974eb8fa1b774e72f9ade821ece17e
Kevadroz <kevinfdezdominguez@gmail.com>
2024-01-25 22:52:26 +0100

    Version 0.1.0
    Signed-off-by: Kevadroz <kevinfdezdominguez@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# NoControllerGlyphs
+
+A Geometry Dash Geode mod that disables gamepad indicators.
+
+I (probably) don't know what I am doing.
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CCSprite.hpp>
+#include <Geode/modify/GameToolbox.hpp>
+
+using namespace geode::prelude;
+
+
+std::unordered_set<std::string> cGlyphs = {
+	// Reference only "controllerBtn_DPad_Left_001.png",
+	// Reference only "controllerBtn_DPad_Right_001.png",
+	"controllerBtn_Start_001.png",
+	"controllerBtn_B_001.png",
+	"controllerBtn_X_001.png",
+	"controllerBtn_RThumb_001.png",
+	"controllerBtn_A_001.png",
+	"controllerBtn_LThumb_001.png",
+	"controllerBtn_DPad_Down_001.png",
+	"controllerBtn_Y_001.png"
+};
+
+
+class $modify(CCSprite) {
+	static CCSprite* createWithSpriteFrameName(char const* pszSpriteFrameName) {
+		CCSprite* sprite;
+		if (strcmp("controllerBtn_DPad_Left_001.png", pszSpriteFrameName) == 0)
+			sprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
+		else if (strcmp("controllerBtn_DPad_Right_001.png", pszSpriteFrameName) == 0) {
+			sprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
+			sprite->setFlipX(true);
+		} else {
+			sprite = CCSprite::createWithSpriteFrameName(pszSpriteFrameName);
+
+		if (cGlyphs.find(pszSpriteFrameName) != cGlyphs.end())
+			sprite->setVisible(false);
+		}
+
+		return sprite;
+	}
+};
+
+
+class $modify(MenuLayer) {
+
+bool init() {
+	if (!MenuLayer::init())
+		return false;
+
+	CCObject* object = NULL;
+		CCARRAY_FOREACH(this->getChildren(), object)
+		{
+			if (auto textLabel = typeinfo_cast<CCLabelBMFont*>(object)) {
+				if (strcmp(this->getID().c_str(), "MenuLayer") == 0) {
+					auto text = textLabel->getString();
+					if (strcmp(text, "Mouse") == 0 || strcmp(text, "Click") == 0 ) {
+						textLabel->setVisible(false);
+					}
+				}
+			}
+		}
+
+	return true;
+}
+
+};
+
+
+// Does not work
+class $modify(GameToolbox) {
+
+static void addRThumbScrollButton(CCLayer* param_1) {
+	return; // Removed
+}
+
+};
