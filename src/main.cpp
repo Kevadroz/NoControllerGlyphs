843d10d414cb3f1f19dee0f014156a130cbb01c3
Kevadroz <kevinfdezdominguez@gmail.com>
2024-01-28 00:51:44 +0100

    v0.1.1 Fixed Custom Keybinds incompatibility
    Signed-off-by: Kevadroz <kevinfdezdominguez@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/CCSprite.hpp>
+#include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/GameToolbox.hpp>
 
 using namespace geode::prelude;
@@ -20,72 +20,58 @@ std::unordered_set<std::string> cGlyphs = {
 };
 
 
+bool isLoaded = false;
+
+// This should be in code hell.
 class $modify(CCSprite) {
+
 	static CCSprite* createWithSpriteFrameName(char const* pszSpriteFrameName) {
-		CCSprite* sprite;
-		if (strcmp("controllerBtn_DPad_Left_001.png", pszSpriteFrameName) == 0)
-			sprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
-		else if (strcmp("controllerBtn_DPad_Right_001.png", pszSpriteFrameName) == 0) {
-			sprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
-			sprite->setFlipX(true);
+		if (isLoaded) {
+
+			CCSprite* sprite;
+			if (strcmp("controllerBtn_DPad_Left_001.png", pszSpriteFrameName) == 0)
+				sprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
+			else if (strcmp("controllerBtn_DPad_Right_001.png", pszSpriteFrameName) == 0) {
+				sprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
+				sprite->setFlipX(true);
+			} else {
+				sprite = CCSprite::createWithSpriteFrameName(pszSpriteFrameName);
+
+			if (cGlyphs.find(pszSpriteFrameName) != cGlyphs.end())
+				sprite->setVisible(false);
+				
+			}
+
+			return sprite;
 		} else {
-			sprite = CCSprite::createWithSpriteFrameName(pszSpriteFrameName);
-
-		if (cGlyphs.find(pszSpriteFrameName) != cGlyphs.end())
-			sprite->setVisible(false);
+			return CCSprite::createWithSpriteFrameName(pszSpriteFrameName);
 		}
-
-		return sprite;
 	}
+
+};
+
+
+class $modify(GameToolbox) {
+
+	static void addRThumbScrollButton(CCLayer* param_1) {
+		return;
+	}
+
 };
 
 
 class $modify(MenuLayer) {
 
-bool init() {
-	if (!MenuLayer::init())
-		return false;
+	bool init() {
+		isLoaded = true;
 
-	CCObject* object = NULL;
-		CCARRAY_FOREACH(this->getChildren(), object)
-		{
-			if (auto textLabel = typeinfo_cast<CCLabelBMFont*>(object)) {
-				if (strcmp(this->getID().c_str(), "MenuLayer") == 0) {
-					auto text = textLabel->getString();
-					if (strcmp(text, "Mouse") == 0 || strcmp(text, "Click") == 0 ) {
-						textLabel->setVisible(false);
-					}
-				}
-			}
-		}
+		if (!MenuLayer::init())
+			return false;
 
-	return true;
-}
+		this->getChildByID("mouse-gamepad-label")->setVisible(false);
+		this->getChildByID("click-gamepad-label")->setVisible(false);
+
+		return true;
+	}
 
 };
-
-
-// Bindings do not exist
-// class $modify(GameToolbox) {
-
-// static void addRThumbScrollButton(CCLayer* param_1) {
-// 	return; // Removed
-// }
-
-// };
-
-
-// Manual hook
-void GameToolbox_addRThumbScrollButton() {
-	return;
-}
-
-// While bindings for this function do not exist this is here
-$execute {
-	Mod::get()->hook(
-		reinterpret_cast<void*>(geode::base::get() + 0x419c0),
-		&GameToolbox_addRThumbScrollButton,
-		"GameToolbox::addRThumbScrollButton",
-		tulip::hook::TulipConvention::Thiscall
-	);
-}
